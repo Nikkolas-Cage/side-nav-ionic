cecbb61bc114aff1c04daa6eabc01d64bcce0d98
Nikkolas-Cage <47923060+Nikkolas-Cage@users.noreply.github.com>
2022-04-18 17:37:39 +0800

    update

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,13 +1,55 @@
 import './ExploreContainer.css';
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-creative";
+
+import { EffectCreative } from "swiper";
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
 
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
-    <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
+    <div>
+      
+      <Swiper
+      initialSlide={2}
+        grabCursor={true}
+        effect={"creative"}
+        creativeEffect={{
+          prev: {
+            shadow: true,
+            translate: [0, 0, -400],
+          },
+          next: {
+            translate: ["53%", 0, 0],
+          },
+        }}
+        modules={[EffectCreative]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="sideNav">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, in autem quas eius quaerat corporis fuga incidunt. Aut veniam incidunt quisquam? Maxime dolore sit omnis quis, aliquid corrupti obcaecati ex!
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='main'>
+            
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Blank</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, optio tempore at dicta, sequi nobis quas quis, accusamus enim fuga quisquam saepe repudiandae rerum eveniet illo voluptatum doloribus quod. Nobis?
+          </div>
+        </SwiperSlide>
+      </Swiper>
+       </div>
   );
 };
 
